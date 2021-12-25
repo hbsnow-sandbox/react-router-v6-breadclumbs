e873b69243c46c6e69b8fffc38d618203d75dffd
hbsnow <yuki.4uing@gmail.com>
2021-12-25 11:39:35 +0900

    feat: css追加

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from "react";
 
+import classes from "./index.module.css";
+
 type Props = Readonly<PropsWithChildren<unknown>>;
 
 export const DefaultLayout = (props: Props): JSX.Element => {
@@ -11,7 +13,7 @@ export const DefaultLayout = (props: Props): JSX.Element => {
         <h1>Title</h1>
       </header>
       <nav>
-        <ul>
+        <ul className={classes.menu}>
           <li>
             <a href="/">Home</a>
           </li>
